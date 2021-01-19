8ec93f8f949f5eb663d461f5ea066a96249f8369
RingEric <younglin495@gmail.com>
2021-01-19 18:46:06 +0800

    Maven支持

---
+++ src/main/scala/qm/qiaomeng/entry/Entry.scala
@@ -0,0 +1,13 @@
+package qm.qiaomeng.entry
+
+/**
+ * @ClassName: Entry
+ * @Description: TODO
+ * @Create by: LinYoung
+ * @Date: 2021/1/19 18:01
+ */
+object Entry {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
