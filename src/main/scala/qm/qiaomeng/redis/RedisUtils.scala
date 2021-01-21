bf64f0bedd3445863fab163f6bcc505936587340
RingEric <younglin495@gmail.com>
2021-01-21 14:07:32 +0800

    往redis中写入一条数据并设置过期时间

--- src/main/scala/qm/qiaomeng/redis/RedisUtils.scala
+++ src/main/scala/qm/qiaomeng/redis/RedisUtils.scala
@@ -9,11 +9,19 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
  * @Date: 2021/1/12 15:39
  */
 object RedisUtils {
-  def writeToRedis(key: String, value: String): String = {
+
+  /**
+   * 往redis中写入一条数据并设置过期时间
+   * @param key 存入的key
+   * @param value 存入的值
+   * @param milliseconds 过期的毫秒值
+   * @return 返回存入信息
+   */
+  def writeToRedis(key: String, value: String, milliseconds: Long): String = {
     val client: StatefulRedisClusterConnection[String, String] = RedisClient.getRedisClient
 
     // 同步存入redis
-    val reply = client.sync.set(key, value)
+    val reply = client.sync.psetex(key, milliseconds, value)
     reply
   }
 }
